d52a00e07b42b77354b397db5326bf7c8190cbdf
Tim <mideltim@gmail.com>
2025-06-11 22:20:54 +0800

    feat: add logo and avatar icon in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,22 +2,23 @@
 
 import Link from "next/link";
 import { useAuth } from "@/context/AuthContext";
+import Avatar from "./ui/avatar";
 
 export default function Navbar() {
-  const { logout } = useAuth();
-
+  const { user, logout } = useAuth();
+  if (!user) return null;
   return (
-    <header className="w-full bg-card mb-10">
-      <nav className="flex justify-between items-center p-4 mx-4">
+    <header className="w-full bg-card mb-5">
+      <nav className="flex justify-between items-center p-2 mx-4">
         <Link href="/" className="hover:text-foreground/80">
-          Home
+          <Avatar src="/blue_logo.png" alt="profile picture" size={50} />
         </Link>
 
         <button
           onClick={logout}
           className="hover:text-foreground/80 cursor-pointer"
         >
-          Logout
+          <Avatar src={user.avatarUrl} alt="profile picture" size={45} />
         </button>
       </nav>
     </header>
